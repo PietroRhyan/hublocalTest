d5a7faf300787ec6ab73c0d176318cd815211497
Pietro Rhyan <pietroqjg@gmail.com>
2022-01-17 20:22:39 -0300

    Next Build

--- src/pages/_documents.tsx
+++ src/pages/_documents.tsx
@@ -1,5 +1,5 @@
+import React from 'react'
 import Document, { DocumentContext, DocumentInitialProps, Html, Main, NextScript } from 'next/document'
-import Head from 'next/head'
 import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
